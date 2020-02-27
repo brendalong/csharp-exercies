77dc8d36092e51429b83a06e42d318c83beaf8b7
Brenda Long <brenda@studio6thave.com>
2020-02-27 14:46:44 -0600

    code-alongs

---
+++ MyApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyApp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string greeting = "Hello World";
+      int numberTacos = 12;
+      bool hasMeat = true;
+      // string[] tacoFlavors = ["cheese", "chicken", "beef"];
+      //but hey arrays are dead to us so lets use...
+      List<string> tacoFlavors = new List<string>() { "cheese", "chicken", "beef" };
+
+      for (int i = 0; i < tacoFlavors.Count; i++)
+      {
+        Console.WriteLine(tacoFlavors[i]);
+      }
+
+      foreach (var item in tacoFlavors)
+      {
+        Console.WriteLine("foreach " + item);
+      }
+
+      if (numberTacos > 5)
+      {
+        Console.Write("woa thats a lot of tacos");
+      }
+      else
+      {
+        Console.WriteLine("eat more tacos")
+      }
+
+      Console.WriteLine($"{greeting} I'd like {numberTacos} tacos and meat={hasMeat}");
+    }
+  }
+}
---
+++ Planner/Building.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Planner
+{
+  // Define class here
+  public class Building
+  {
+
+    // private types
+    private string _designer;
+    private DateTime _dateConstructed = DateTime.Now;
+    private string _address;
+    private string _owner;
+
+  }
+
+}
