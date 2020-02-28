a3315ab13c833fcda440048c28ec498ebead37ed
Brenda Long <brenda@studio6thave.com>
2020-02-28 10:03:25 -0600

    intro to list and created worddictionary practice

---
+++ ListIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListIntro {
+  class Program {
+    static void Main(string[] args) {
+      List<string> nssCohorts = new List<string> {
+        "Cohort 36",
+        "Cohort 37",
+        "Cohort 38"
+      };
+
+      nssCohorts.Add("Cohort 39");
+
+      List<int> birdYears = new List<int> {
+        1996,
+        2001,
+        1842
+      };
+
+      birdYears.Add(2000);
+
+      foreach (var item in nssCohorts) {
+        Console.WriteLine(item);
+      }
+
+      birdYears.Remove(1996);
+
+      foreach (var year in birdYears) {
+        Console.WriteLine(year);
+
+        if (year == 1996) {
+          Console.WriteLine("Holden was born");
+        } else {
+          Console.WriteLine("no no");
+        }
+      }
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ WordsDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WordsDictionary
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+Create a dictionary with key value pairs to
+represent words (key) and its definition (value)
+*/
+      Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+      // Add several more words and their definitions
+      wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+      wordsAndDefinitions.Add("Taco", "Variable name implying 'it could be anything'");
+      wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+
+      /*
+          Use square brackets to get the definition of two of the
+          words and then output them to the console
+      */
+
+      /*
+          Below, loop over the wordsAndDefinitions dictionary to get the following output:
+              The definition of (WORD) is (DEFINITION)
+              The definition of (WORD) is (DEFINITION)
+              The definition of (WORD) is (DEFINITION)
+      */
+      foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+      {
+      }
+    }
+  }
+}
