af142b74b14f8ce2694a4ce0cdb5232b8dcb26f3
Brenda Long <brenda@studio6thave.com>
2020-03-06 14:42:21 -0600

    work with linq

--- LINQed/Program.cs
+++ LINQed/Program.cs
@@ -1,18 +1,204 @@
 ﻿using System;
 using System.Collections.Generic;
-using LINQed;
+using System.Linq;
 
-namespace LINQed {
-  class Program {
-    static void Main (string[] args) {
-      Console.WriteLine ("Just a linqed up stuff");
+namespace LINQed
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Just some linq-ed up stuff");
 
-      List<string> fruits = new List<string> () { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+      List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
       IEnumerable<string> LFruits = from fruit in fruits
-      where find fruit[0] == "L"
-      orderby count ascending
-      select count;
+      where fruit.StartsWith('L')
+      select fruit;
+
+      // Which of the following numbers are multiples of 4 or 6
+      List<int> numbers = new List<int>()
+      {
+        15,
+        8,
+        21,
+        24,
+        32,
+        13,
+        30,
+        12,
+        7,
+        54,
+        48,
+        4,
+        49,
+        96
+      };
+
+      IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+
+      // Order these student names alphabetically, in descending order (Z to A)
+      List<string> names = new List<string>()
+      {
+        "Heather",
+        "James",
+        "Xavier",
+        "Michelle",
+        "Brian",
+        "Nina",
+        "Kathleen",
+        "Sophia",
+        "Amir",
+        "Douglas",
+        "Zarley",
+        "Beatrice",
+        "Theodora",
+        "William",
+        "Svetlana",
+        "Charisse",
+        "Yolanda",
+        "Gregorio",
+        "Jean-Paul",
+        "Evangelina",
+        "Viktor",
+        "Jacqueline",
+        "Francisco",
+        "Tre"
+      };
+
+      List<string> descend = (from name in names orderby name descending select name).ToList();
+      IEnumerable<string> descend2 = names.OrderByDescending(name => name);
+
+      // Build a collection of these numbers sorted in ascending order
+      List<int> numberList = new List<int>()
+      {
+        15,
+        8,
+        21,
+        24,
+        32,
+        13,
+        30,
+        12,
+        7,
+        54,
+        48,
+        4,
+        49,
+        96
+      };
+
+      IEnumerable<int> numberListAscending = numberList.OrderByDescending(num => num);
+
+      //----------------------------- //
+      // Output how many numbers are in this list
+      List<int> howmanyNumbers = new List<int>()
+      {
+        15,
+        8,
+        21,
+        24,
+        32,
+        13,
+        30,
+        12,
+        7,
+        54,
+        48,
+        4,
+        49,
+        96
+      };
+
+      Console.WriteLine($"Howmany Nums {howmanyNumbers.Count}");
+
+      // How much money have we made?
+      //m stands for decimal
+      List<decimal> purchases = new List<decimal>
+      {
+        2340.29m,
+        745.31m,
+        21.76m,
+        34.03m,
+        4786.45m,
+        879.45m,
+        9442.85m,
+        2454.63m,
+        45.65m
+      };
+
+      Console.WriteLine($"All the Money {purchases.Sum()}");
+      // decimal sum = (from x in purchases select x).Sum();
+      //for each multiply by 100 to remove decimal then add together(sum) then
+      //divide by 100
+
+      List<double> purchases2 = new List<double>
+      {
+        2340.29,
+        745.31,
+        21.76,
+        34.03,
+        4786.45,
+        879.45,
+        9442.85,
+        2454.63,
+        45.65
+      };
+      IEnumerable<double> allNumbs = from count in purchases2
+      select count * 100;
+
+      var allMoneyAgain = Math.Round(allNumbs.Sum() * .01, 2);
+
+      List<double> prices = new List<double>()
+      {
+        879.45,
+        9442.85,
+        2454.63,
+        45.65,
+        2340.29,
+        34.03,
+        4786.45,
+        745.31,
+        21.76
+      };
+
+      Console.WriteLine($"Max Value is {prices.Max()}");
+
+      List<int> wheresSquaredo = new List<int>()
+      {
+        66,
+        12,
+        8,
+        27,
+        82,
+        34,
+        7,
+        50,
+        19,
+        46,
+        81,
+        23,
+        30,
+        4,
+        68,
+        14
+      };
+      /*
+          Store each number in the following List until a perfect square
+          is detected.
+
+          Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 }
+
+          Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+      */
+
+      //first write out to determine is sqrRoot
+      int testVal = 1;
+      if (testVal == 0 || testVal == 1)
+      {
+        Console.WriteLine($"The Test Val {testVal} has No SquareRoot");
+      }
+
     }
   }
 }
---
+++ LinqIntro/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqIntro
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var nssStudents = new List<Student>()
+            {
+                new Student()
+                {
+                Name = "Spencer",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Heidi",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Namita",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Holden",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Aryn",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Aryn",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Jansen",
+                CohortName = "Day Cohort 37"
+                }
+            };
+
+      var names = nssStudents.Select(student =>
+      {
+        return student.Name;
+      });
+
+      //AKA
+      var names2 = nssStudents.Select(student => student.Name);
+
+
+      var studentDescription = nssStudents.Select(student =>
+      {
+        return ($"{student.Name} is a memeber of {student.CohortName}");
+      });
+
+      var students37 = nssStudents.Where(student =>
+      {
+        return student.CohortName == "Day Cohort 37";
+      });
+
+      /////////// adam do ////////////
+      // List<int> cohortStudentCount = new List<int>()
+      //   {
+      //       25, 12, 28, 22, 11, 25, 27, 24, 19
+      //   };
+
+      // /*
+      //     Now we need to determine which cohorts fell within the range
+      //     of acceptable size - between 20 and 26 students. Also, sort
+      //     the new enumerable collection in ascending order.
+      // */
+      // IEnumerable<int> idealSizes = from count in cohortStudentCount
+      //                               where count < 27 && count > 19
+      //                               orderby count ascending
+      //                               select count;
+
+      // cohortStudentCount.Where(count < 27 && cohortStudentCount > 19)
+      // .Orderby(count)
+
+      // // Display each number that was the acceptable size
+      // foreach (int c in idealSizes)
+      // {
+      //   Console.WriteLine($"{c}");
+      // }
+
+      //find the first one
+      var spencer = nssStudents.FirstOrDefault(student =>
+      {
+        // return student.Name == "Spencer";
+        return student.Name.Contains("pen");
+      });
+
+      var hasStudentsFrom45 = nssStudents.Any(student =>
+      {
+        return student.CohortName == "Day Cohort 45";
+      });
+
+      // var whatAllExists = nssStudents.GroupBy()
+
+      var countdown = nssStudents
+      .GroupBy(student => student.CohortName)
+      .Select(group =>
+      {
+        return new CohortReport
+        {
+          StudentCount = group.Count(),
+          CohortName = group.Key
+        };
+      });
+
+      foreach (var item in countdown)
+      {
+        Console.WriteLine($"name {item.CohortName}");
+      }
+    }
+  }
+}
+
+public class CohortReport
+{
+  public string CohortName { get; set; }
+  public int StudentCount { get; set; }
+}
