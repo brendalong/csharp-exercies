63e63f16e9e2dd7b8e3fe646ab37bdc2375b37b6
Brenda Long <brenda@studio6thave.com>
2020-03-13 11:17:48 -0500

    start the heist

--- HeistPartII/Program.cs
+++ HeistPartII/Program.cs
@@ -2,10 +2,12 @@
 
 namespace HeistPartII
 {
+  //https://gist.github.com/AdamSheaffer/c7563b750ab767f2123fd868e022dc67
   class Program
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("do i work");
       Bank GreenBank = new Bank();
 
     }
